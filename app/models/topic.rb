20f0e6146bdc871bfa947c1102b1de336199d200
Jacqui Maher <jacqui@streeteasy.com>
2008-10-23 01:55:57 -0400

    last_poster method

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,12 +1,16 @@
 class Topic < ActiveRecord::Base    
   belongs_to :user
   belongs_to :forum
-  has_many :posts, :dependent => :destroy, :order => "posts.created_at asc"
+  has_many :posts, :dependent => :destroy, :order => "posts.created_at desc"
   has_many :users, :through => :posts
-  belongs_to :last_post, :class_name => "Post"
   
   validates_associated :posts, :message => nil
   validates_length_of :title, :minimum => 4
   validates_presence_of :title, :forum_id, :user_id
   validates_associated :user
+
+  def last_poster
+    self.users.first
+  end
+  
 end
